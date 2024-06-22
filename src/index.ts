23329066b5025baa7dca897d607dba42123f903f
Marisol Nuñez <marisol280602@hotmail.com>
2024-06-21 20:18:04 -0600

    Primer commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+
+class Server {
+  //TODO: crear la instancia global de nuestra app.
+  public app: Application;
+
+  //!TO DO: Generar el constructor
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  // TO DO: Generar un método para la configuración
+
+  config(): void {
+    // * TODO: Configuracion de puerto para el server
+
+    this.app.set("port", process.env.PORT || 3000);
+
+    // * TODO: Mostrar las peticiones en conosla
+    this.app.use(morgan("dev"));
+
+    // * TODO: Uso de cors (Cross Origin)
+    this.app.use(cors());
+
+    //* TODO: Generar restricción a la API
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
+
+  // ! TO DO: Generar un método para la configuración de rutas
+
+  private routes(): void {
+    throw new Error("Not Implemented");
+  }
+
+  // TO DO: Generar un método para inicializar el servicio
+
+  start(): void {
+    this.app.listen(this.app.get("port"), () => {
+      console.log("Server on port", this.app.get("port"));
+    });
+  }
+}
+
+const server = new Server();
+server.start();
