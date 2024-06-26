5ba95d45f6227e7417002ebb23a1dfafbe15e7a0
Marisol Nuñez <marisol280602@hotmail.com>
2024-06-25 20:30:23 -0600

    Configuracion de swagger UI

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -9,10 +9,66 @@ class IndexRoutes {
     this.config();
   }
 
+  /**
+   *
+   */
+
   config(): void {
+    /**
+     * @swagger
+     * /api:
+     *  get:
+     *      tags: ["index"]
+     *      summary: Defaul Index
+     *      description: Ruta por defecto de la API
+     *      produces:
+     *          - application/json
+     *      responses:
+     *          200:
+     *              description: Exitoso
+     */
     this.router.get("/", indexController.index);
+    /**
+     * @swagger
+     * /api:
+     *  post:
+     *      tags: ["index"]
+     *      summary: Defaul Index
+     *      description: Ruta por defecto de la API
+     *      produces:
+     *          - application/json
+     *      responses:
+     *          200:
+     *              description: Exitoso
+     */
     this.router.post("/", indexController.insert);
+    /**
+     * @swagger
+     * /api:
+     *  put:
+     *      tags: ["index"]
+     *      summary: Defaul Index
+     *      description: Ruta por defecto de la API
+     *      produces:
+     *          - application/json
+     *      responses:
+     *          200:
+     *              description: Exitoso
+     */
     this.router.put("/", indexController.update);
+    /**
+     * @swagger
+     * /api:
+     *  delete:
+     *      tags: ["index"]
+     *      summary: Defaul Index
+     *      description: Ruta por defecto de la API
+     *      produces:
+     *          - application/json
+     *      responses:
+     *          200:
+     *              description: Exitoso
+     */
     this.router.delete("/", indexController.delete);
   }
 }
