d6cab5f3223721fa53cce3d2a09fa7d3cc9508fe
Marisol Nuñez <marisol280602@hotmail.com>
2024-07-05 19:30:11 -0600

    configuracion de middleware y utils

--- src/controller/index.controllers.ts
+++ src/controller/index.controllers.ts
@@ -1,21 +1,26 @@
 import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
 import prisma from "../database/database";
+import { utils } from "../utils/utils";
 
 class IndexController {
   public async index(req: Request, res: Response) {
     try {
       const usuarios = await prisma.usuario.findMany();
       //return res.json({ message: "API Works" });
-      const newUser = await prisma.usuario.create({
-        data: {
-          nombre: "Marisol",
-          apelldidos: "nuñez",
-          username: "marisol02",
-          password: "linux",
-        },
-      });
-      return res.json(usuarios);
+      const user = {
+        cveUsuario: 1,
+        nombre: "marisol",
+        rol: [1, 2, 3],
+      };
+      const token = utils.generateJWT(user);
+      console.log(token);
+      let jwt =
+        " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdmVVc3VhcmlvIjoxLCJub21icmUiOiJtYXJpc29sIiwicm9sIjpbMSwyLDNdLCJpYXQiOjE3MjAyMjc1NTcsImV4cCI6MTcyMDIzMTE1N30.geucHl59QuOOdK7ak__UTFQm-MSRcfUUs-RPBD2TSi8";
+
+      let data = utils.getPayload(jwt);
+      return res.json({ message: "API Words" });
+      console.log(data);
     } catch (error: any) {
       return res.status(500).json({ message: `Error ${error.message}` });
     }
--- src/middlewares/jwt.check.ts
+++ src/middlewares/jwt.check.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { Jwt } from "jsonwebtoken";
+import { utils } from "../utils/utils";
 
 export const jwtCheck = (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = <string>req.headers["auth"];
 
     //Todo: obtener la informacion del token
-    //let payload
+    let payload = utils.getPayload(token);
 
     //TODO: refreshToken
+
+    const newToken = utils.generateJWT(payload);
+    res.setHeader("auth", newToken);
 
     next();
   } catch (error) {
